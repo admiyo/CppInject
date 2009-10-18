0fd0459946695b920180d044007bdff66af9ab53
mlarouche <larouche@kde.org>
2009-10-18 14:46:16 -0400

    Forget license header

--- include/Zone.h
+++ include/Zone.h
@@ -1,3 +1,8 @@
+// Copyright 2009 Michaël Larouche <larouche@kde.org>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 #ifndef _CPPINJECT_ZONE_H_
 #define _CPPINJECT_ZONE_H_
 
