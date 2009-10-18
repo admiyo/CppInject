a8b80dc39bb2261d0e1ac1211a4f2e67595c6fe8
mlarouche <larouche@kde.org>
2009-10-18 14:51:11 -0400

    Extract out activator

---
+++ include/activator.h
@@ -0,0 +1,61 @@
+// Copyright 2009 Michaël Larouche <larouche@kde.org>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+#ifndef _CPPINJECT_ACTIVATOR_H_
+#define _CPPINJECT_ACTIVATOR_H_
+
+// TODO: Remove
+#include "Zone.h"
+
+namespace dependency
+{
+
+template <class T>
+T* default_factory(Zone& f)
+{
+	return new T;
+};
+
+template <class T>
+void default_cleaner(T* t)
+{
+	delete t;
+};
+
+template <class T>
+class activator
+{
+public:
+	typedef T* (*factory)(Zone& f) ;
+	typedef void (*cleanup)(T*) ;
+
+	activator()
+	{
+		factory_ = 0;
+		cleanup_ = 0;
+	}
+
+	void operator=(const activator& other)
+	{
+		factory_ = other.factory_;
+		cleanup_ = other.cleanup_;
+	}
+
+	activator(factory f, cleanup c)
+	  : factory_(f), cleanup_(c)
+	{
+	}
+
+	activator(const activator& a)
+	  : factory_(a.factory_), cleanup_(a.cleanup_)
+	{}
+
+	factory factory_;
+	cleanup cleanup_;
+};
+	
+}
+
+#endif
--- include/resolver.h
+++ include/resolver.h
@@ -14,47 +14,10 @@
 #include "GlobalZoneType.h"
 #include "ConcreteZoneType.h"
 #include "Zone.h"
+#include "activator.h"
 
 namespace dependency
 {
-  template <class T>
-    T* default_factory(Zone& f){
-    return new T;
-  };
-
-  template <class T>
-    void default_cleaner(T* t){
-    delete t;
-  };
-  
-
-  template <class T>
-    class activator {
-    public:
-      typedef T* (*factory)(Zone& f) ;
-      typedef void (*cleanup)(T*) ;
-      
-      activator(){
-	factory_ = 0;
-	cleanup_ = 0;
-      };
-      void operator = (const activator& other){
-	factory_ = other.factory_;
-	cleanup_ = other.cleanup_;
-      }
-      
-    activator(factory  f, cleanup  c):
-      factory_(f), cleanup_(c){};
-      
-    activator(const activator& a):
-      factory_(a.factory_), cleanup_(a.cleanup_){};
-      
-      factory  factory_;
-      cleanup  cleanup_;
-    };
-
-
-
   /**
    *
    */
